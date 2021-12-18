621021b04d984abd2049af78f97e192c035d21fd
noxiii <noxiii@nippox.de>
2021-12-18 10:21:45 +0000

    run autopep8

--- notify.py
+++ notify.py
@@ -60,7 +60,8 @@ class NextCloudTalkNotificationService(BaseNotificationService):
         self._session.headers.update({'OCS-APIRequest': 'true'})
         self._session.headers.update({'Accept': 'application/json'})
         """ Get Token/ID for Room """
-        self.caps = self._session.get(f"{url}/ocs/v1.php/cloud/capabilities").json()
+        self.caps = self._session.get(
+            f"{url}/ocs/v1.php/cloud/capabilities").json()
         self.attachments_folder = self.caps["ocs"]["data"]["capabilities"]["spreed"]["config"]["attachments"]['folder']
         self.attachments_allowed = self.caps["ocs"]["data"]["capabilities"]["spreed"]["config"]["attachments"][
             'allowed']
@@ -90,7 +91,7 @@ class NextCloudTalkNotificationService(BaseNotificationService):
                 for attach in data:
                     file = open(data[attach], 'rb')
                     resp = self._session.put(f"{self.url}/{self.webdav_root}{self.attachments_folder}/{attach}",
-                                              data=file)
+                                             data=file)
                     if resp.status_code in (200, 201, 202, 204):
                         uploaded[attach] = data[attach]
                     else:
@@ -107,17 +108,22 @@ class NextCloudTalkNotificationService(BaseNotificationService):
                         share_url = f"{self.url}/ocs/v2.php/apps/files_sharing/api/v1/shares"
                         resp = self._session.post(share_url, data=data)
                         if resp.status_code != 200:
-                            _LOGGER.error("Share file %s error for %s, %s, %s, %s", uploaded_file, target, share_url, resp, data)
+                            _LOGGER.error("Share file %s error for %s, %s, %s, %s",
+                                          uploaded_file, target, share_url, resp, data)
 
                     data = {"token": roomtoken, "message": message, "actorType": "",
                             "actorId": "", "actorDisplayName": "", "timestamp": 0, "messageParameters": []}
                     prefix = "/ocs/v2.php/apps/spreed/api/v1"
-                    resp = self._session.post(f"{self.url}{prefix}/chat/{roomtoken}", data=data)
+                    resp = self._session.post(
+                        f"{self.url}{prefix}/chat/{roomtoken}", data=data)
                     if resp.status_code == 201:
                         success = resp.json()["ocs"]["meta"]["status"]
                         if not success:
-                            _LOGGER.error("Unable to post NextCloud Talk message")
+                            _LOGGER.error(
+                                "Unable to post NextCloud Talk message")
                     else:
-                        _LOGGER.error("Incorrect status code when posting message: %d", resp.status_code)
+                        _LOGGER.error(
+                            "Incorrect status code when posting message: %d", resp.status_code)
                 else:
-                    _LOGGER.error("Unable to post NextCloud Talk message: no token for: %s", target)
+                    _LOGGER.error(
+                        "Unable to post NextCloud Talk message: no token for: %s", target)
